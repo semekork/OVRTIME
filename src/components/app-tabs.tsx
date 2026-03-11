274ef015655b76fa3fc12ced7f78c44f7f1b3239
Caleb Dussey <119165434+semekork@users.noreply.github.com>
2026-03-11 10:07:37 +0000

    Update app-tabs.tsx

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -1,6 +1,6 @@
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { NativeTabs } from "expo-router/unstable-native-tabs";
-import React from "react";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
+import React from 'react';
 
 const { Icon, Label, VectorIcon } = NativeTabs.Trigger;
 
@@ -8,37 +8,24 @@ export default function TabLayout() {
   return (
     <NativeTabs tintColor="#FF6B00">
       <NativeTabs.Trigger name="index">
-        <Icon
-          sf="house.fill"
-          src={<VectorIcon family={MaterialIcons} name="home" />}
-        />
+        <Icon sf="house.fill" src={<VectorIcon family={MaterialIcons} name="home" />} />
         <Label>Home</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="favorites">
-        <Icon
-          sf="star.fill"
-          src={<VectorIcon family={MaterialIcons} name="star" />}
-        />
+        <Icon sf="star.fill" src={<VectorIcon family={MaterialIcons} name="star" />} />
         <Label>Favorites</Label>
       </NativeTabs.Trigger>
 
-       <NativeTabs.Trigger name="news">
-        <Icon
-          sf="newspaper.fill"
-          src={<VectorIcon family={MaterialIcons} name="newspaper" />}
-        />
+      <NativeTabs.Trigger name="news">
+        <Icon sf="newspaper.fill" src={<VectorIcon family={MaterialIcons} name="newspaper" />} />
         <Label>News</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="search" role="search">
-        <Icon
-          sf="magnifyingglass"
-          src={<VectorIcon family={MaterialIcons} name="search" />}
-        />
+        <Icon sf="magnifyingglass" src={<VectorIcon family={MaterialIcons} name="search" />} />
         <Label>Search</Label>
       </NativeTabs.Trigger>
-
     </NativeTabs>
   );
 }
