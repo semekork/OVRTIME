b41e9be9fb40097a9e8b5c84afd7ec753100e39a
Caleb Dussey <119165434+semekork@users.noreply.github.com>
2026-03-11 10:07:21 +0000

    Update _layout.tsx

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,32 +1,6 @@
-import { DarkTheme, ThemeProvider } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import * as SystemUI from "expo-system-ui";
-import React, { useEffect } from "react";
-import { Appearance } from "react-native";
-
-import { AnimatedSplashOverlay } from "@/components/animated-icon";
-import AppTabs from "@/components/app-tabs";
-
-const customDarkTheme = {
-  ...DarkTheme,
-  colors: {
-    ...DarkTheme.colors,
-    background: "#000000",
-    card: "#000000",
-  },
-};
+import React from 'react';
+import AppTabs from '@/components/app-tabs';
 
 export default function TabLayout() {
-  useEffect(() => {
-    Appearance.setColorScheme("dark");
-    SystemUI.setBackgroundColorAsync("#000000");
-  }, []);
-
-  return (
-    <ThemeProvider value={customDarkTheme}>
-      <StatusBar style="light" />
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+  return <AppTabs />;
 }
